48367b5a9f58c1995b6a416d91d8f824d151214e
sadewasadewal <130735842+sadewasadewal@users.noreply.github.com>
2024-03-06 00:51:47 +0530

    web store
    Web store for themes and fonts. User can download and install themes and fonts using this web store.

---
+++ HomePage/WebStore/store.js
@@ -0,0 +1,11 @@
+// Toggle User Menu
+function toggleUserMenu() {
+    var userMenu = document.getElementById("user-menu");
+    userMenu.classList.toggle("show");
+}
+
+// Toggle Cart
+function toggleCart() {
+    var cart = document.getElementById("cart");
+    cart.classList.toggle("show");
+}
