1c1b4dfd7a56032993a05d71caf5fe1f383c1308
Wizardatgit <159108003+Wizardatgit@users.noreply.github.com>
2024-03-21 20:46:06 +0530

    Made 6 card components we are going to put in

--- my-react-app/src/Pages/FontPage.jsx
+++ my-react-app/src/Pages/FontPage.jsx
@@ -1,10 +1,36 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import exampleImage from '../assets/Images/sample.jpg';
+import './FontPage.css';
 
-export default function FontPage(){
+
+function Card({fontimageurl, title}){
+    return(
+        <div className="fontcard">
+            <img src={fontimageurl} alt="firstfont" className="fontimg"></img>
+            <h2 className="fontcard-title">{title}</h2>
+            <p>download</p>
+        </div>
+            );
+}
+
+
+
+function FontPage(){
     return(
         <div>
             <Navbar></Navbar>
-            <h1>Fontpage</h1>
+            <div className="cardcontainer">
+                <Card fontimageurl={exampleImage} title={"Firstfont"}/>    
+                <Card fontimageurl={exampleImage} title={"Secondfont"}/>    
+                <Card fontimageurl={exampleImage} title={"Thirdfont"}/>    
+                <Card fontimageurl={exampleImage} title={"Fourthfont"}/>    
+                <Card fontimageurl={exampleImage} title={"Fifthfont"}/>  
+                <Card fontimageurl={exampleImage} title={"Sixthfont"}/>      
+            </div>
+            
+           
         </div>
-    )
+    );
 }
+
+export default FontPage;
--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar(){
     return <nav className="nav">
-        <Link href="/" className="site-title">Site Name</Link>
+        <Link href="/" className="site-title">Chromacraft</Link>
         <ul>
             <li>
                 <Link to="/"> Home</Link>
