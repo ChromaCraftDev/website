b8795797e32d8d22a690592bcc4d8c0555bb5429
Thanuga-Rodrigo <thanuganethushan@gmail.com>
2024-03-22 13:15:34 +0530

    commit

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',git add .
-    git commit -m "commit"
-    git push
+    outDir: 'build',
   }
 })
